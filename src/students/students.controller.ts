309b5487d2780112d0b592b69998c009478f0329
Dhirajzurule12 <zuruledhiraj@gmail.com>
2022-08-16 00:13:01 +0530

    create students.service.ts file

---
+++ src/students/students.controller.ts
@@ -0,0 +1,10 @@
+import { Controller,Post} from "@nestjs/common";
+
+@Controller('students')
+export class StudentsController{
+       @post()
+       addStudent():any{
+
+        
+       }
+}
---
+++ src/students/students.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable
+expoert class StudentsService{
+    
+
+}
