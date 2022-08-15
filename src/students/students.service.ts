6035bdcf97c1ad4c0e1ab4b390fd3b8ea7ef8d2c
Dhirajzurule12 <zuruledhiraj@gmail.com>
2022-08-16 00:47:39 +0530

    added insertStudent Method

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import{Students} from './students.model';
+@Injectable()
+export class StudentsService{
+    student:Students[]=[];
 
-@Injectable
-expoert class StudentsService{
-    
+    insertStudent(Name:string, Address:string,MobileNo:number){
+        const newStudent= new Students(new Date().toString(),Name ,Address,MobileNo)
+    }
+
+    this.student.push(newStudent);
+
+
 
 }
